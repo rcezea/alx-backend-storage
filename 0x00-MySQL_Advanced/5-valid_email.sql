4b0f4df950725ebb0e24cf1440c6b2b5dd8982b5
Richard <rcezea007@gmail.com>
2024-05-09 18:31:16 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,6 @@
 -- create trigger to update email validation
 CREATE TRIGGER validate AFTER UPDATE ON users
     FOR EACH ROW
-    UPDATE users SET valid_email = 0 WHERE OLD.email != email
+    IF OLD.email != NEW.email THEN
+    UPDATE users SET valid_email = 0 WHERE id = NEW.id;
+    END IF
