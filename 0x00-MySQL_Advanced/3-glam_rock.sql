6d9a52a575dde4d284cb49691d505235088b7240
Richard <rcezea007@gmail.com>
2024-05-09 17:53:33 +0100

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- all bands with Glam rock as main style
-SELECT band_name, (IFNULL(split, 2022) - formed) AS 'lifespan' from metal_bands ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed) AS 'lifespan' from metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
