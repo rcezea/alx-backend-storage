e1e07e627960fa06f821acff4c2e04392e6fa463
Richard <rcezea007@gmail.com>
2024-05-09 18:59:18 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,11 @@
 -- create trigger to update email validation
 DROP TRIGGER IF EXISTS validate;
+DELIMITER $$ ;
 CREATE TRIGGER validate BEFORE UPDATE
     ON users FOR EACH ROW
     BEGIN
         IF !(NEW.email <=> OLD.email) THEN
             SET NEW.valid_email = 0;
         END IF;
-    END
+    END;
+DELIMITER ; $$
