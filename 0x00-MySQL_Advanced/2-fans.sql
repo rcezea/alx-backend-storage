3f43298f5a6fa604af5e339d79762b5473a47f90
Richard <rcezea007@gmail.com>
2024-05-09 17:15:29 +0100

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,1 +1,2 @@
-SELECT origin, sum(fans) AS 'nb_fans' FROM metal_bands GROUP BY origin ORDER BY nb_fans desc;
+-- compute sum of fans
+SELECT origin, sum(fans) AS 'nb_fans' FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
